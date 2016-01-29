88cc0ca97bde04e093c55ca4c8da42d031007d30
Mike Desrochers <miked@jori.ca>
2016-01-29 14:38:10 -0700

    Move Bullet spawning into a Pattern subclass. Consider removing the children field from Pattern, it may not be needed

--- core/src/com/udacity/desromj/kerfuffle/entity/Player.java
+++ core/src/com/udacity/desromj/kerfuffle/entity/Player.java
@@ -2,24 +2,12 @@ package com.udacity.desromj.kerfuffle.entity;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
-import com.udacity.desromj.kerfuffle.bullet.PlayerBullet;
 import com.udacity.desromj.kerfuffle.pattern.PlayerBulletPattern;
 import com.udacity.desromj.kerfuffle.screen.GameScreen;
 import com.udacity.desromj.kerfuffle.utility.Assets;
 import com.udacity.desromj.kerfuffle.utility.Constants;
-
-import spine.AnimationState;
-import spine.AnimationStateData;
-import spine.Skeleton;
-import spine.SkeletonData;
-import spine.SkeletonJson;
-import spine.SkeletonRenderer;
-import spine.SkeletonRendererDebug;
 
 /**
  * Created by Mike on 2016-01-27.
@@ -30,14 +18,18 @@ public class Player
 
     float shotDelay, cannotShootFor;
 
-    Pattern shotPattern;
+    Pattern bulletPattern;
 
     public Player(Vector2 position)
     {
         this.position = new Vector2(position.x, position.y);
         this.shotDelay = 1.0f / Constants.PLAYER_SHOTS_PER_SECOND;
         this.cannotShootFor = 0.0f;
-        // this.shotPattern = new PlayerBulletPattern();
+
+        this.bulletPattern = new PlayerBulletPattern(
+                new Vector2(this.position.x, this.position.y),
+                new Vector2(0, 0)
+        );
     }
 
     public void shootBullets(GameScreen screen)
@@ -45,32 +37,7 @@ public class Player
         if (cannotShootFor <= 0.0f)
         {
             cannotShootFor = shotDelay;
-
-            Spawnable bullet1 = new PlayerBullet(
-                    new Vector2(this.position.x - Constants.PLAYER_RADIUS * 4, this.position.y + Constants.PLAYER_RADIUS),
-                    new Vector2(0.0f, Constants.PLAYER_SHOT_SPEED));
-
-            Spawnable bullet2 = new PlayerBullet(
-                    new Vector2(this.position.x - Constants.PLAYER_RADIUS * 2, this.position.y + Constants.PLAYER_RADIUS * 3),
-                    new Vector2(0.0f, Constants.PLAYER_SHOT_SPEED));
-
-            Spawnable bullet3 = new PlayerBullet(
-                    new Vector2(this.position.x, this.position.y + Constants.PLAYER_RADIUS * 5),
-                    new Vector2(0.0f, Constants.PLAYER_SHOT_SPEED));
-
-            Spawnable bullet4 = new PlayerBullet(
-                    new Vector2(this.position.x + Constants.PLAYER_RADIUS * 2, this.position.y + Constants.PLAYER_RADIUS * 3),
-                    new Vector2(0.0f, Constants.PLAYER_SHOT_SPEED));
-
-            Spawnable bullet5 = new PlayerBullet(
-                    new Vector2(this.position.x + Constants.PLAYER_RADIUS * 4, this.position.y + Constants.PLAYER_RADIUS),
-                    new Vector2(0.0f, Constants.PLAYER_SHOT_SPEED));
-
-            screen.addSpawnable(bullet1);
-            screen.addSpawnable(bullet2);
-            screen.addSpawnable(bullet3);
-            screen.addSpawnable(bullet4);
-            screen.addSpawnable(bullet5);
+            screen.addSpawnables(this.bulletPattern.spawnChildren(this.position));
         }
     }
 
--- core/src/com/udacity/desromj/kerfuffle/pattern/PlayerBulletPattern.java
+++ core/src/com/udacity/desromj/kerfuffle/pattern/PlayerBulletPattern.java
@@ -3,26 +3,51 @@ package com.udacity.desromj.kerfuffle.pattern;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.DelayedRemovalArray;
+import com.udacity.desromj.kerfuffle.bullet.PlayerBullet;
 import com.udacity.desromj.kerfuffle.entity.Pattern;
 import com.udacity.desromj.kerfuffle.entity.Spawnable;
+import com.udacity.desromj.kerfuffle.utility.Constants;
 
 /**
  * Created by Quiv on 2016-01-29.
  */
 public class PlayerBulletPattern extends Pattern
 {
-    public PlayerBulletPattern(Vector2 position, Vector2 velocity) {
-        super(position, velocity);
-    }
+    public PlayerBulletPattern(Vector2 position, Vector2 velocity) { super(position, velocity); }
 
     @Override
     public void render(ShapeRenderer renderer)
     {
-
+        renderer.setColor(Constants.PLAYER_SHOT_COLOR);
+        renderer.circle(this.position.x, this.position.y, Constants.PLAYER_SHOT_RADIUS);
     }
 
     @Override
-    public Array<Spawnable> spawnChildren(Vector2 origin) {
-        return null;
+    public Array<Spawnable> spawnChildren(Vector2 origin)
+    {
+        Array<Spawnable> spawns = new DelayedRemovalArray<Spawnable>();
+
+        spawns.add(new PlayerBullet(
+                new Vector2(origin.x - Constants.PLAYER_RADIUS * 4, origin.y + Constants.PLAYER_RADIUS),
+                new Vector2(0.0f, Constants.PLAYER_SHOT_SPEED)));
+
+        spawns.add(new PlayerBullet(
+                new Vector2(origin.x - Constants.PLAYER_RADIUS * 2, origin.y + Constants.PLAYER_RADIUS * 3),
+                new Vector2(0.0f, Constants.PLAYER_SHOT_SPEED)));
+
+        spawns.add(new PlayerBullet(
+                new Vector2(origin.x, origin.y + Constants.PLAYER_RADIUS * 5),
+                new Vector2(0.0f, Constants.PLAYER_SHOT_SPEED)));
+
+        spawns.add(new PlayerBullet(
+                new Vector2(origin.x + Constants.PLAYER_RADIUS * 2, origin.y + Constants.PLAYER_RADIUS * 3),
+                new Vector2(0.0f, Constants.PLAYER_SHOT_SPEED)));
+
+        spawns.add(new PlayerBullet(
+                new Vector2(origin.x + Constants.PLAYER_RADIUS * 4, origin.y + Constants.PLAYER_RADIUS),
+                new Vector2(0.0f, Constants.PLAYER_SHOT_SPEED)));
+
+        return spawns;
     }
 }
