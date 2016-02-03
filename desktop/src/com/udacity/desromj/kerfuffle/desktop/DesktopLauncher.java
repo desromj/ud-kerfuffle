294dd949e75f67b918ebdb92196998aa8586ee66
Mike Desrochers <desrochers.m.j@gmail.com>
2016-02-03 00:23:40 -0700

    Change default window size for desktop

--- desktop/src/com/udacity/desromj/kerfuffle/desktop/DesktopLauncher.java
+++ desktop/src/com/udacity/desromj/kerfuffle/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.udacity.desromj.kerfuffle.KerfuffleGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.width = 600;
+		config.height = 960;
+
 		new LwjglApplication(new KerfuffleGame(), config);
 	}
 }
