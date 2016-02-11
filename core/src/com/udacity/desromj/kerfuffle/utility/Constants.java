2a2da6f7fbe3114adadba321905044b58d7f3645
Mike Desrochers <miked@jori.ca>
2016-02-11 14:37:06 -0700

    Edit Constants to create values from the WORLD SIZE, instead of hard numbers. May be needing to change the scale later

--- core/src/com/udacity/desromj/kerfuffle/utility/Constants.java
+++ core/src/com/udacity/desromj/kerfuffle/utility/Constants.java
@@ -8,20 +8,21 @@ import com.badlogic.gdx.graphics.Color;
 public class Constants
 {
     public static final float WORLD_WIDTH = 520.0f;
-    public static final float WORLD_HEIGHT = 760.0f;
+
+    public static final float WORLD_HEIGHT = WORLD_WIDTH * 1.5f;
 
     public static final String GAME_TITLE = "A-Pollen";
 
     public static final float PLAYER_HEALTH = 1.0f;
-    public static final float PLAYER_RADIUS = 4.0f;
-    public static final float PLAYER_SPEED = 320.0f;
-    public static final float PLAYER_FOCUS_SPEED = 160.0f;
+    public static final float PLAYER_RADIUS = WORLD_WIDTH / 130.0f;
+    public static final float PLAYER_SPEED = WORLD_WIDTH * 0.6f;
+    public static final float PLAYER_FOCUS_SPEED = PLAYER_SPEED / 2.0f;
 
     public static final float PLAYER_SHOTS_PER_SECOND = 12.0f;
     public static final Color PLAYER_SHOT_COLOR = Color.YELLOW;
-    public static final float PLAYER_SHOT_RADIUS = 4.0f;
-    public static final float PLAYER_SHOT_SEPARATION = 12.0f;
-    public static final float PLAYER_SHOT_SPEED = 1200.0f;
+    public static final float PLAYER_SHOT_RADIUS = PLAYER_RADIUS;
+    public static final float PLAYER_SHOT_SEPARATION = WORLD_WIDTH / 40.0f;
+    public static final float PLAYER_SHOT_SPEED = WORLD_WIDTH * 2.0f;
     public static final float PLAYER_SHOT_DAMAGE = 2.0f;
 
     public static final Color BACKGROUND_COLOR = Color.BLUE;
@@ -31,24 +32,24 @@ public class Constants
      */
     public static final float ENEMY_FLY_HEALTH = 60.0f;
     public static final Color ENEMY_FLY_COLOUR = Color.RED;
-    public static final float ENEMY_FLY_RADIUS = 16.0f;
+    public static final float ENEMY_FLY_RADIUS = WORLD_WIDTH / 30.0f;
 
     public static final float ENEMY_FLY_SHOTS_PER_SECOND = 0.4f;
-    public static final float ENEMY_FLY_SHOT_SPEED = 400.0f;
+    public static final float ENEMY_FLY_SHOT_SPEED = WORLD_WIDTH * 0.75f;
 
     /*
     Bullet values
      */
     public static final Color BULLET_SMALL_RED_PELLET_COLOR_BORDER = Color.RED;
     public static final Color BULLET_SMALL_RED_PELLET_COLOR_INNER = Color.WHITE;
-    public static final float BULLET_SMALL_RED_PELLET_RADIUS = 5.0f;
-    public static final float BULLET_SMALL_RED_PELLET_MARGIN = 2.0f;
+    public static final float BULLET_SMALL_RED_PELLET_RADIUS = WORLD_WIDTH / 100.0f;
+    public static final float BULLET_SMALL_RED_PELLET_MARGIN = WORLD_WIDTH / 250.0f;
     public static final float BULLET_SMALL_RED_PELLET_DAMAGE = 2.0f;
 
     public static final Color BULLET_LARGE_YELLOW_BALL_COLOR_BORDER = Color.YELLOW;
     public static final Color BULLET_LARGE_YELLOW_BALL_COLOR_INNER = Color.WHITE;
-    public static final float BULLET_LARGE_YELLOW_BALL_RADIUS = 20.0f;
-    public static final float BULLET_LARGE_YELLOW_BALL_MARGIN = 4.0f;
+    public static final float BULLET_LARGE_YELLOW_BALL_RADIUS = WORLD_WIDTH / 26.0f;
+    public static final float BULLET_LARGE_YELLOW_BALL_MARGIN = WORLD_WIDTH / 125.0f;
     public static final float BULLET_LARGE_YELLOW_BALL_DAMAGE = 4.0f;
 
 
