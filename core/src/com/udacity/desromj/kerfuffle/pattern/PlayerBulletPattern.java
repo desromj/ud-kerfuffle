778c4154e8542e87cd1dbb34c9e077c9a054a46e
Mike Desrochers <miked@jori.ca>
2016-03-03 12:48:26 -0700

    Narrow Player's bullet pattern when focussed

--- core/src/com/udacity/desromj/kerfuffle/pattern/PlayerBulletPattern.java
+++ core/src/com/udacity/desromj/kerfuffle/pattern/PlayerBulletPattern.java
@@ -1,5 +1,7 @@
 package com.udacity.desromj.kerfuffle.pattern;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.DelayedRemovalArray;
@@ -7,6 +9,7 @@ import com.udacity.desromj.kerfuffle.bullet.SpawnFactory;
 import com.udacity.desromj.kerfuffle.entity.Bullet;
 import com.udacity.desromj.kerfuffle.entity.Pattern;
 import com.udacity.desromj.kerfuffle.entity.PatternProperties;
+import com.udacity.desromj.kerfuffle.entity.Player;
 import com.udacity.desromj.kerfuffle.entity.Shooter;
 import com.udacity.desromj.kerfuffle.entity.Spawnable;
 import com.udacity.desromj.kerfuffle.utility.Constants;
@@ -27,18 +30,31 @@ public class PlayerBulletPattern extends Pattern
     {
         Array<Spawnable> spawns = new DelayedRemovalArray<Spawnable>();
 
-        Vector2 origin = this.getParent().getPosition();
+        Player player = (Player) this.getParent();
+
+        Vector2 origin = player.getPosition();
+        boolean isFocussed = Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT);
 
         spawns.add(getBullet(
-                new Vector2(origin.x - Constants.PLAYER_SHOT_SEPARATION * 4, origin.y + Constants.PLAYER_SHOT_SEPARATION)));
+                new Vector2(
+                        (isFocussed) ? origin.x - Constants.PLAYER_SHOT_SEPARATION * 2.0f : origin.x - Constants.PLAYER_SHOT_SEPARATION * 4.0f,
+                        (isFocussed) ? origin.y + Constants.PLAYER_SHOT_SEPARATION * 0.5f : origin.y + Constants.PLAYER_SHOT_SEPARATION * 1.0f)));
         spawns.add(getBullet(
-                new Vector2(origin.x - Constants.PLAYER_SHOT_SEPARATION * 2, origin.y + Constants.PLAYER_SHOT_SEPARATION * 3)));
+                new Vector2(
+                        (isFocussed) ? origin.x - Constants.PLAYER_SHOT_SEPARATION * 1.0f : origin.x - Constants.PLAYER_SHOT_SEPARATION * 2.0f,
+                        (isFocussed) ? origin.y + Constants.PLAYER_SHOT_SEPARATION * 1.5f : origin.y + Constants.PLAYER_SHOT_SEPARATION * 3.0f)));
         spawns.add(getBullet(
-                new Vector2(origin.x, origin.y + Constants.PLAYER_SHOT_SEPARATION * 5)));
+                new Vector2(
+                        origin.x,
+                        (isFocussed) ? origin.y + Constants.PLAYER_SHOT_SEPARATION * 2.5f : origin.y + Constants.PLAYER_SHOT_SEPARATION * 5.0f)));
         spawns.add(getBullet(
-                new Vector2(origin.x + Constants.PLAYER_SHOT_SEPARATION * 2, origin.y + Constants.PLAYER_SHOT_SEPARATION * 3)));
+                new Vector2(
+                        (isFocussed) ? origin.x + Constants.PLAYER_SHOT_SEPARATION * 1.0f : origin.x + Constants.PLAYER_SHOT_SEPARATION * 2.0f,
+                        (isFocussed) ? origin.y + Constants.PLAYER_SHOT_SEPARATION * 1.5f : origin.y + Constants.PLAYER_SHOT_SEPARATION * 3.0f)));
         spawns.add(getBullet(
-                new Vector2(origin.x + Constants.PLAYER_SHOT_SEPARATION * 4, origin.y + Constants.PLAYER_SHOT_SEPARATION)));
+                new Vector2(
+                        (isFocussed) ? origin.x + Constants.PLAYER_SHOT_SEPARATION * 2.0f : origin.x + Constants.PLAYER_SHOT_SEPARATION * 4.0f,
+                        (isFocussed) ? origin.y + Constants.PLAYER_SHOT_SEPARATION * 0.5f : origin.y + Constants.PLAYER_SHOT_SEPARATION * 1.0f)));
 
         return spawns;
     }
