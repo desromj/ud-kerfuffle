a3745b528978e6375d1d54d67ffa40e0c0498e8a
Mike Desrochers <miked@jori.ca>
2016-03-09 13:58:52 -0700

    Set up skeleton for the tough enemy as well

--- core/src/com/udacity/desromj/kerfuffle/enemy/MantisEnemy.java
+++ core/src/com/udacity/desromj/kerfuffle/enemy/MantisEnemy.java
@@ -4,44 +4,42 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.udacity.desromj.kerfuffle.entity.Enemy;
 import com.udacity.desromj.kerfuffle.entity.Pattern;
-import com.udacity.desromj.kerfuffle.utility.Constants;
 
 /**
  * Created by Mike on 2016-03-09.
  */
-public class FlyEnemy extends Enemy
+public class MantisEnemy extends Enemy
 {
-    public FlyEnemy(Vector2 position, float screenActivationHeight)
+
+    public MantisEnemy(Vector2 position, float screenActivationHeight)
     {
-        this(position, screenActivationHeight, new Pattern[]{});
+        super(position, screenActivationHeight);
         // TODO: set asset loader here when sprites are ready
     }
 
-    public FlyEnemy(Vector2 position, float screenActivationHeight, Pattern[] patterns)
+    public MantisEnemy(Vector2 position, float screenActivationHeight, Pattern[] patterns)
     {
         super(position, screenActivationHeight, patterns);
         // TODO: set asset loader here when sprites are ready
     }
 
     @Override
+    public void loadDefaultPattern() {
+
+    }
+
+    @Override
     protected void setHealth() {
-        this.health = Constants.ENEMY_FLY_HEALTH;
+
     }
 
     @Override
     protected void setHitRadius() {
-        this.hitRadius = Constants.ENEMY_FLY_RADIUS;
-    }
-
-    @Override
-    public void render(SpriteBatch batch)
-    {
 
     }
 
     @Override
-    public void loadDefaultPattern()
-    {
+    public void render(SpriteBatch batch) {
 
     }
 }
